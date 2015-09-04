4737ed42654593396725c138ad4d4d55c0fc7d97
李言 <liyan8@letvpicture.com>
2015-09-04 21:09:16 +0800

    默认的迁移方式 关闭自动推断

---
+++ CoreData/Amount.h
@@ -0,0 +1,17 @@
+//
+//  Amount.h
+//  
+//
+//  Created by 李言 on 15/9/4.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Amount : NSManagedObject
+
+@property (nonatomic, retain) NSString * xyz;
+
+@end
---
+++ CoreData/Amount.m
@@ -0,0 +1,16 @@
+//
+//  Amount.m
+//  
+//
+//  Created by 李言 on 15/9/4.
+//
+//
+
+#import "Amount.h"
+
+
+@implementation Amount
+
+@dynamic xyz;
+
+@end
